3ed0876f8d39d355f416e566c25cae49ec36ab0f
Samuel Johansson <samueljohanssonhue@gmail.com>
2018-08-12 14:36:41 +0200

    Added redux-logger

--- react-app/src/store/store.ts
+++ react-app/src/store/store.ts
@@ -1,9 +1,20 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-function configureStore(initialState?: {}) {
-  return createStore(rootReducer, initialState!);
+function configureStore(initialState = {}) {
+  const middleware = [];
+
+  if (process.env.NODE_ENV !== 'production') {
+    middleware.push(logger);
+  }
+
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middleware)
+  );
 }
 
 const store = configureStore();
