1f991e47a76fefd7ffa21caa275271f52ecf3ba5
Samuel Johansson <@animech.com>
2020-07-04 14:39:53 +0200

    refactor: remove sample app

--- new-react-app/src/index.tsx
+++ new-react-app/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import "./index.css";
-import { App } from "./App";
+import { App } from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
 render(
