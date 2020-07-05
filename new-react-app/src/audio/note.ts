f4dfd7bbc01b08b3fdb77beaa252245dbfb9688e
Samuel Johansson <@animech.com>
2020-07-05 16:31:47 +0200

    refactor(AudioSettings): refactor using hooks

--- new-react-app/src/audio/note.ts
+++ new-react-app/src/audio/note.ts
@@ -1,16 +1,22 @@
-export type NoteString =
-  | "A"
-  | "A#"
-  | "B"
-  | "C"
-  | "C#"
-  | "D"
-  | "D#"
-  | "E"
-  | "F"
-  | "F#"
-  | "G"
-  | "G#";
+export const noteStrings = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+] as const;
+
+export const isNoteString = (value: string): value is NoteString =>
+  noteStrings.some((v) => v === value);
+
+export type NoteString = typeof noteStrings[number];
 
 export class Note {
   public constructor(
--- new-react-app/src/features/synth/components/AudioSettings.tsx
+++ new-react-app/src/features/synth/components/AudioSettings.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {
-  StyleRulesCallback,
   Checkbox,
   FormControl,
   Input,
@@ -11,18 +10,16 @@ import {
   MenuItem,
   Select,
   TextField,
-  Theme,
-  WithStyles,
-  withStyles,
+  makeStyles,
   Slider,
 } from "@material-ui/core";
 import { debounce } from "ts-debounce";
-import { NoteString } from "../../../audio";
+import { NoteString, noteStrings, isNoteString } from "../../../audio";
 import { PlayButton } from "../../../components";
 import { RootState } from "../../../store";
 import { synthActions, synthSelectors } from "..";
 
-const styles: StyleRulesCallback<Theme, any> = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(2),
   },
@@ -50,9 +47,13 @@ const styles: StyleRulesCallback<Theme, any> = (theme) => ({
     paddingBottom: 8,
     textAlign: "center",
   },
-});
+}));
 
-type AudioSettingsProps = {
+const octaveMinMin = 1;
+const octaveMaxMax = 7;
+const notesErrorText = "Select notes";
+
+export type AudioSettingsProps = {
   autoPlay: boolean;
   ambience: number;
   bpm: number;
@@ -67,239 +68,142 @@ type AudioSettingsProps = {
   updateOctaveMax: (octave: number) => any;
 };
 
-type CssClasses =
-  | "root"
-  | "button"
-  | "formControl"
-  | "rangeContainer"
-  | "rangeFrom"
-  | "rangeTo"
-  | "rangeSeparator";
-type PropsWithStyles = AudioSettingsProps & WithStyles<CssClasses>;
+const CoreAudioSettings: React.FC<AudioSettingsProps> = ({
+  autoPlay,
+  ambience,
+  bpm,
+  notes,
+  octaveMin,
+  octaveMax,
+  toggleAutoPlay,
+  updateAmbience,
+  updateBpm,
+  selectNotes,
+  updateOctaveMin,
+  updateOctaveMax,
+}) => {
+  const classes = useStyles();
 
-type State = {
-  ambience: number;
-  bpm: number;
+  return (
+    <div className={classes.root}>
+      <PlayButton
+        className={classes.button}
+        fullWidth
+        playing={autoPlay}
+        onTogglePlaying={toggleAutoPlay}
+      >
+        Auto play
+      </PlayButton>
+      <FormControl className={classes.formControl} fullWidth>
+        Ambience
+        <Slider
+          value={ambience}
+          onChange={(_, value) => updateAmbience(value as number)}
+          valueLabelDisplay="auto"
+          step={1}
+          min={0}
+          max={100}
+        />
+      </FormControl>
+      <FormControl className={classes.formControl} fullWidth>
+        BPM
+        <Slider
+          value={bpm}
+          onChange={(_, value) => updateBpm(value as number)}
+          valueLabelDisplay="auto"
+          step={1}
+          min={60}
+          max={300}
+        />
+      </FormControl>
+      <FormControl className={classes.formControl} fullWidth>
+        <InputLabel htmlFor="notes">Notes</InputLabel>
+        <Select
+          multiple
+          fullWidth
+          error={notes.length === 0}
+          value={notes.length > 0 ? notes : [notesErrorText]}
+          input={<Input id="notes" />}
+          onChange={(e) =>
+            selectNotes((e.target.value as string[]).filter(isNoteString))
+          }
+          renderValue={(values) => (values as string[]).join(", ")}
+        >
+          {noteStrings.map((note) => (
+            <MenuItem key={note} value={note}>
+              <Checkbox color="primary" checked={notes.indexOf(note) > -1} />
+              <ListItemText primary={note} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl} fullWidth>
+        <InputLabel htmlFor="octave-from" shrink>
+          Octave range
+        </InputLabel>
+        <div className={classes.rangeContainer}>
+          <div className={classes.rangeFrom}>
+            <TextField
+              fullWidth
+              label=" "
+              type="number"
+              value={octaveMin}
+              onChange={(e) => {
+                let value = Number(e.target.value);
+                if (value < octaveMinMin) {
+                  value = octaveMinMin;
+                } else if (value > octaveMax) {
+                  value = octaveMax;
+                }
+                updateOctaveMin(value);
+              }}
+              inputProps={{ min: octaveMinMin, max: octaveMax, step: 1 }}
+            />
+          </div>
+          <div className={classes.rangeSeparator}>-</div>
+          <div className={classes.rangeTo}>
+            <TextField
+              fullWidth
+              label=" "
+              type="number"
+              value={octaveMax}
+              onChange={(e) => {
+                let value = Number(e.target.value);
+                if (value < octaveMin) {
+                  value = octaveMin;
+                } else if (value > octaveMaxMax) {
+                  value = octaveMaxMax;
+                }
+                updateOctaveMax(value);
+              }}
+              inputProps={{ min: octaveMin, max: octaveMaxMax, step: 1 }}
+            />
+          </div>
+        </div>
+      </FormControl>
+    </div>
+  );
 };
 
-class CoreAudioSettings extends React.Component<PropsWithStyles, State> {
-  private allNotes: NoteString[] = [
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-  ];
-  private notesErrorText = "Select notes";
-  private octaveMinMin = 1;
-  private octaveMaxMax = 7;
-
-  public constructor(props: PropsWithStyles) {
-    super(props);
-
-    this.state = {
-      ambience: props.ambience || 0,
-      bpm: props.bpm || 0,
-    };
-
-    this.renderNotesValue = this.renderNotesValue.bind(this);
-    this.handleAmbienceChange = this.handleAmbienceChange.bind(this);
-    this.handleBpmChange = this.handleBpmChange.bind(this);
-    this.handleNotesChange = this.handleNotesChange.bind(this);
-    this.handleOctaveMinChange = this.handleOctaveMinChange.bind(this);
-    this.handleOctaveMaxChange = this.handleOctaveMaxChange.bind(this);
-  }
-
-  public static getDerivedStateFromProps(
-    nextProps: PropsWithStyles,
-    prevState: State,
-  ) {
-    const state: Partial<State> = {};
-    if (nextProps.ambience !== prevState.ambience) {
-      state.ambience = nextProps.ambience;
-    }
-    if (nextProps.bpm !== prevState.bpm) {
-      state.bpm = nextProps.bpm;
-    }
-    return state;
-  }
-
-  public render() {
-    const {
-      classes,
-      autoPlay,
-      notes,
-      octaveMin,
-      octaveMax,
-      toggleAutoPlay,
-    } = this.props;
-    const { ambience, bpm } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <PlayButton
-          className={classes.button}
-          fullWidth
-          playing={autoPlay}
-          onTogglePlaying={toggleAutoPlay}
-        >
-          Auto play
-        </PlayButton>
-        <FormControl className={classes.formControl} fullWidth>
-          Ambience
-          <Slider
-            value={ambience}
-            onChange={this.handleAmbienceChange}
-            step={1}
-            min={0}
-            max={100}
-          />
-        </FormControl>
-        <FormControl className={classes.formControl} fullWidth>
-          BPM
-          <Slider
-            value={bpm}
-            onChange={this.handleBpmChange}
-            step={1}
-            min={60}
-            max={300}
-          />
-        </FormControl>
-        <FormControl className={classes.formControl} fullWidth>
-          <InputLabel htmlFor="notes">Notes</InputLabel>
-          <Select
-            multiple
-            fullWidth
-            error={notes.length === 0}
-            value={notes.length > 0 ? notes : [this.notesErrorText]}
-            input={<Input id="notes" />}
-            onChange={this.handleNotesChange}
-            renderValue={this.renderNotesValue}
-          >
-            {this.allNotes.map((note) => (
-              <MenuItem key={note} value={note}>
-                <Checkbox color="primary" checked={notes.indexOf(note) > -1} />
-                <ListItemText primary={note} />
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl className={classes.formControl} fullWidth>
-          <InputLabel htmlFor="octave-from" shrink>
-            Octave range
-          </InputLabel>
-          <div className={classes.rangeContainer}>
-            <div className={classes.rangeFrom}>
-              <TextField
-                fullWidth
-                label=" "
-                type="number"
-                value={octaveMin}
-                onChange={this.handleOctaveMinChange}
-                inputProps={{ min: this.octaveMinMin, max: octaveMax, step: 1 }}
-              />
-            </div>
-            <div className={classes.rangeSeparator}>-</div>
-            <div className={classes.rangeTo}>
-              <TextField
-                fullWidth
-                label=" "
-                type="number"
-                value={octaveMax}
-                onChange={this.handleOctaveMaxChange}
-                inputProps={{ min: octaveMin, max: this.octaveMaxMax, step: 1 }}
-              />
-            </div>
-          </div>
-        </FormControl>
-      </div>
-    );
-  }
-
-  private renderNotesValue(selected: unknown) {
-    return (selected as NoteString[]).join(", ");
-  }
-
-  private handleAmbienceChange(e: any, value: number | number[]) {
-    this.setState({ ambience: value as number });
-    this.handleAmbienceChangeDelayed();
-  }
-
-  private handleAmbienceChangeDelayed = debounce(() => {
-    const { updateAmbience } = this.props;
-    const { ambience } = this.state;
-    updateAmbience(ambience ? ambience : 0);
-  }, 200);
-
-  private handleBpmChange(e: any, value: number | number[]) {
-    this.setState({ bpm: value as number });
-    this.handleBpmChangeDelayed();
-  }
-
-  private handleBpmChangeDelayed = debounce(() => {
-    const { updateBpm } = this.props;
-    const { bpm } = this.state;
-    updateBpm(bpm ? bpm : 0);
-  }, 200);
-
-  private handleNotesChange(e: any) {
-    const { selectNotes } = this.props;
-    selectNotes(
-      e.target.value.filter((value: string) => value !== this.notesErrorText),
-    );
-  }
-
-  private handleOctaveMinChange(e: any) {
-    const { updateOctaveMin, octaveMax } = this.props;
-    let value = Number(e.target.value);
-    if (value < this.octaveMinMin) {
-      value = this.octaveMinMin;
-    } else if (value > octaveMax) {
-      value = octaveMax;
-    }
-    updateOctaveMin(value);
-  }
-
-  private handleOctaveMaxChange(e: any) {
-    const { updateOctaveMax, octaveMin } = this.props;
-    let value = Number(e.target.value);
-    if (value < octaveMin) {
-      value = octaveMin;
-    } else if (value > this.octaveMaxMax) {
-      value = this.octaveMaxMax;
-    }
-    updateOctaveMax(value);
-  }
-}
-
-export const AudioSettings = withStyles(styles)(
-  connect(
-    (state: RootState) => ({
-      autoPlay: synthSelectors.getAutoPlay(state.synth),
-      ambience: synthSelectors.getAmbience(state.synth),
-      bpm: synthSelectors.getBpm(state.synth),
-      notes: synthSelectors.getNotes(state.synth),
-      octaveMin: synthSelectors.getOctaveMin(state.synth),
-      octaveMax: synthSelectors.getOctaveMax(state.synth),
-    }),
-    (dispatch) =>
-      bindActionCreators(
-        {
-          toggleAutoPlay: synthActions.toggleAutoPlay,
-          updateAmbience: synthActions.updateAmbience,
-          updateBpm: synthActions.updateBpm,
-          selectNotes: synthActions.selectNotes,
-          updateOctaveMin: synthActions.updateOctaveMin,
-          updateOctaveMax: synthActions.updateOctaveMax,
-        },
-        dispatch,
-      ),
-  )(CoreAudioSettings),
-);
+export const AudioSettings = connect(
+  (state: RootState) => ({
+    autoPlay: synthSelectors.getAutoPlay(state.synth),
+    ambience: synthSelectors.getAmbience(state.synth),
+    bpm: synthSelectors.getBpm(state.synth),
+    notes: synthSelectors.getNotes(state.synth),
+    octaveMin: synthSelectors.getOctaveMin(state.synth),
+    octaveMax: synthSelectors.getOctaveMax(state.synth),
+  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        toggleAutoPlay: synthActions.toggleAutoPlay,
+        updateAmbience: synthActions.updateAmbience,
+        updateBpm: synthActions.updateBpm,
+        selectNotes: synthActions.selectNotes,
+        updateOctaveMin: synthActions.updateOctaveMin,
+        updateOctaveMax: synthActions.updateOctaveMax,
+      },
+      dispatch,
+    ),
+)(CoreAudioSettings);
